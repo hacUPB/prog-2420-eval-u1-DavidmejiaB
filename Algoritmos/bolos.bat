c47ffb38ce5362aaf284e45973cefc660670d752
David <david.mejiabetancur@upb.edu.co>
2024-08-08 16:09:55 -0500

    trabajo 3

---
+++ Algoritmos/bolos.bat
@@ -0,0 +1,16 @@
+Iniciar
+Definir puntos totales como 0
+leer numero de pinos derrumbados en turno 1
+leer numero de pinos derrumbados en turno 2
+leer numero de pinos derrumbados en turno 3
+leer numero de pinos derrumbados en turno 4
+leer numero de pinos derrumbados en turno 5
+leer numero de pinos derrumbados en turno 6
+leer numero de pinos derrumbados en turno 7
+leer numero de pinos derrumbados en turno 8
+leer numero de pinos derrumbados en turno 9
+leer numero de pinos derrumbados en turno 10
+para todos los datos registrados del 1 al 10 hacer:
+Sumar Turno 1, Turno 2, Turno 3, Turno 4, Turno 5, Turno 6, Turno 7, Turno 8, Turno 9, Turno 10 a puntos totales
+imprimir puntos totales 
+fin 
