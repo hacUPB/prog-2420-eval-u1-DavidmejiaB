bf00a5795332b0562a349d9710c51e9f6a54f7b9
DavidmejiaB <david.mejiabetancur@upb.edu.co>
2024-08-13 16:25:18 -0500

    bolos.bat

--- Algoritmos/bolos.bat
+++ Algoritmos/bolos.bat
@@ -1,16 +1,17 @@
 Iniciar
 Definir puntos totales como 0
-leer numero de pinos derrumbados en turno 1
-leer numero de pinos derrumbados en turno 2
-leer numero de pinos derrumbados en turno 3
-leer numero de pinos derrumbados en turno 4
-leer numero de pinos derrumbados en turno 5
-leer numero de pinos derrumbados en turno 6
-leer numero de pinos derrumbados en turno 7
-leer numero de pinos derrumbados en turno 8
-leer numero de pinos derrumbados en turno 9
-leer numero de pinos derrumbados en turno 10
+    leer turno 1
+    leer turno 2
+    leer turno 3
+    leer turno 4
+    leer turno 5
+    leer turno 6
+    leer turno 7
+    leer turno 8
+    leer turno 9
+    leer turno 10
 para todos los datos registrados del 1 al 10 hacer:
-Sumar Turno 1, Turno 2, Turno 3, Turno 4, Turno 5, Turno 6, Turno 7, Turno 8, Turno 9, Turno 10 a puntos totales
+    Sumar Turno 1, Turno 2, Turno 3, Turno 4, Turno 5, Turno 6, Turno 7, Turno 8, Turno 9, Turno 10 a puntos totales
+fin para
 imprimir puntos totales 
 fin 
